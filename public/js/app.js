370528059698f9d1a0afeb3efb940fcf943c15ff
NikulinIlya <ilia.nikulin@gmail.com>
2018-12-11 22:09:50 +0300

    fix(routes): make transfer to 404 page from nonexistent url

--- public/js/app.js
+++ public/js/app.js
@@ -30791,7 +30791,7 @@ Vue.use(__WEBPACK_IMPORTED_MODULE_4_vue_progressbar___default.a, {
 
 Vue.use(__WEBPACK_IMPORTED_MODULE_5_vue_router__["a" /* default */]);
 
-var routes = [{ path: '/dashboard', component: __webpack_require__(174) }, { path: '/developer', component: __webpack_require__(177) }, { path: '/users', component: __webpack_require__(180) }, { path: '/profile', component: __webpack_require__(183) }];
+var routes = [{ path: '/dashboard', component: __webpack_require__(174) }, { path: '/developer', component: __webpack_require__(177) }, { path: '/users', component: __webpack_require__(180) }, { path: '/profile', component: __webpack_require__(183) }, { path: '*', component: __webpack_require__(204) }];
 
 var router = new __WEBPACK_IMPORTED_MODULE_5_vue_router__["a" /* default */]({
     mode: 'history',
